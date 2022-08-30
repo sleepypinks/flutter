1d52d9bbf5ebb495b697f134aed9f983ed648f0b
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2022-08-30 10:48:45 -0400

    added recipes file

---
+++ lib/recipes.dart
@@ -0,0 +1,67 @@
+class RecipeData {
+  static final getData = [
+    {
+      "title": "Lemon cheesecake",
+      "price": 3.99,
+      "description": "A cheesecake made with fresh lemon juice and milk",
+      "image":
+          "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/lemoncheese_cake.jpg"
+    },
+    {
+      "title": "Victoria sponge",
+      "price": 2.99,
+      "description": "A light sponge cake made with eggs and sugar",
+      "image":
+          "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/victoria_sponge.jpg"
+    },
+    {
+      "title": "Carrot cake",
+      "price": 1.99,
+      "description": "A carrot cake made with fresh carrot",
+      "image":
+          "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/carrot_cake.jpg"
+    },
+    {
+      "title": "Banana cake",
+      "price": 2.99,
+      "description": "A tasteful and nutty banana cake",
+      "image":
+          "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/banana_cake.jpg"
+    },
+    {
+      "title": "Birthday cake",
+      "price": 3.99,
+      "description": "A vanilla cake with fresh berries",
+      "image":
+          "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/birthday_cake.JPG"
+    },
+    {
+      "title": "Avocado cake",
+      "price": 2.99,
+      "description": "An herbal cake with fresh avocado",
+      "image":
+          "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/avocado_cake.jpg"
+    },
+    {
+      "title": "Chocolate cake",
+      "price": 2.99,
+      "description": "A chocolatey and rick cake",
+      "image":
+          "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/chocolate_cake.jpg"
+    },
+    {
+      "title": "Clementine cake",
+      "price": 2.99,
+      "description": "A citrusy cake with fresh clementine",
+      "image":
+          "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/clementine_cake.png"
+    },
+    {
+      "title": "Fudge cake",
+      "price": 2.99,
+      "description": "A solid and nutty fudge cake",
+      "image":
+          "https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/fudge_cake.jpg"
+    },
+  ];
+}
