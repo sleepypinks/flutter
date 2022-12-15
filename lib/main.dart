c61dd16f4554af10ccd49eeee1b155bb5ddeccbb
Zainab_Iftikhar <zainab_iftikhar@brown.edu>
2022-12-15 08:08:53 +0000

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'menu_data.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:async';
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
@@ -13,10 +17,10 @@ class MyApp extends StatelessWidget {
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Studio - Flutter'),
+      home: MyHomePage(title: 'Space Space for Doggos ❤️'),
     );
   }
 }
@@ -28,10 +32,96 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        appBar: AppBar(
+          // The title text which will be shown on the action bar
+          title: Text(title),
+        ),
+        body: Column(children: [CatButton()]));
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final String name;
+  final String description;
+  final String price;
+  final String imageUrl;
+  const MenuItem(
+      {Key? key,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.imageUrl})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.network(imageUrl),
+          Text(name),
+          Text(description),
+          Text(price),
+          SizedBox(height: 50),
+        ]);
+  }
+}
+
+class CatButton extends StatefulWidget {
+  State<CatButton> createState() => CreateCatButton();
+}
+
+class CreateCatButton extends State<CatButton> {
+  bool clickedState = false;
+  String imageUrl = "";
+  String buttonCaption = "Which Doggo Are You? Click to find out!";
+  //final String buttonCaption = "Which Cat 🐱 Are You? Click to find out!";
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 22));
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const SizedBox(height: 50),
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              getCat(http.Client());
+            },
+            child: Text(buttonCaption),
+          ),
+          if (clickedState) Image.network(imageUrl),
+        ],
       ),
     );
+  }
+
+//I tried so much to run this for cats but my request kept throwing an error! :()
+  void getCat(http.Client client) async {
+    final response =
+        await client.get(Uri.parse('https://dog.ceo/api/breeds/image/random'));
+    //var statusCodes = [100, 101, 200, 205]
+    //final _random = new Random();
+    //var element = list[_random.nextInt(list.length)];
+    //var url = 'https://http.cat/' + '$element'
+    //await client.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        clickedState = true;
+        final parsed = jsonDecode(response.body);
+        imageUrl = parsed['message'].toString();
+        buttonCaption = "Here's your doggo. Try again for more!";
+      });
+    } else {
+      clickedState = false;
+      buttonCaption = "We failed to load your doggo. Try again :(!";
+      throw Exception('Failed to load doggo');
+    }
   }
 }
